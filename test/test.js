79e72fb3c97f4d24742404a213372d00e966628f
Joseph Dykstra <josephdykstra@gmail.com>
2015-12-04 00:15:10 -0600

    Add cURL test

--- test/test.js
+++ test/test.js
@@ -6,26 +6,44 @@ var files = require('test-audio')()
 var Server = require('../server.js')
 var Upload = require('../client.js')
 
-test('ogg file', function (t) {
+test('ogg file using curl', function (t) {
+	var server = Server()
+
+	server.listen(8080, function () {
+		var filePath = files[2].path
+
+		var curl = cp.spawn('curl', [
+			'localhost:8080/upload',
+			'-F', 'lolwut=@' + filePath
+		])
+
+		var size = sizeStream()
+		size.once('size', function (bytes) {
+			t.ok(bytes > 5600 && bytes < 6100, '5600 > ' + bytes + ' > 6100') // this fails because it is looking at the expected size of converting the mp3 file
+
+			server.close()
+			t.end()
+		})
+
+		curl.stdout.pipe(size)
+	})
+})
+
+test('ogg file using the client', function (t) {
 	var server = Server()
 	var upload = Upload()
 
 	server.listen(8080, function () {
-		var filePath = files[3].path
+		var filePath = files[2].path
 		var fileBuffer = fs.readFileSync(filePath)
 		fileBuffer.name = filePath
 
-		//upload(fileBuffer)
-		cp.spawn('curl', [
-			'localhost:8080/upload',
-			'-F', 'herpderp=@node_modules\\test-audio\\audio\\8000__cfork__cf-fx-bloibb.mp3'
-		])
+		upload(fileBuffer)
 
 		upload.on('ready', function (file) {
 			var size = sizeStream()
 			size.once('size', function (bytes) {
-				t.ok(bytes > 5800) // 5860
-				t.ok(bytes < 5900) // 5860
+				t.ok(bytes > 5600 && bytes < 6100, '5600 > ' + bytes + ' > 6100')
 
 				server.close()
 				t.end()
