ded026ccd5f7d369de50a96a0059cc9fb17cb796
Joseph Dykstra <josephdykstra@gmail.com>
2016-02-05 07:40:59 -0600

    Simplify tests

--- test/test.js
+++ test/test.js
@@ -8,31 +8,31 @@ var after = require('after')
 var Server = require('../server.js')
 var Upload = require('../client.js')
 
-var expectedMp3FileSize = [
-	1892,  // 30047__corsica-s__drippy.flac
-	50179, // 50775__smcameron__drips2.ogg
-	2507,  // 75344__neotone__drip2.wav
-	6006   // 8000__cfork__cf-fx-bloibb.mp3
-]
-var expectedMp3FileMd5s = [
-	'72cbe1ec103eee6dba4f0c2ecd022532', // 30047
-	'cbd234bb0baa13c6018ec87929690b1a', // 50775
-	'13d3fa49e665d4c70c24ef6f5731e1c0', // 75344
-	'f8165368cf2e5d5b1a8a07c12505661a'  // 8000
-]
+var expectedMp3FileSize = {
+	flac: 1892,
+	ogg: 50179,
+	wav: 2507,
+	mp3: 6006
+}
+var expectedMp3FileMd5s = {
+	flac: '72cbe1ec103eee6dba4f0c2ecd022532',
+	ogg: 'cbd234bb0baa13c6018ec87929690b1a',
+	wav: '13d3fa49e665d4c70c24ef6f5731e1c0',
+	mp3: 'f8165368cf2e5d5b1a8a07c12505661a'
+}
 
 test('wav [using curl]', function (t) {
 	setupServer(t, 2, function (next) {
-		var filePath = files[2].path
+		var filePath = files.wav.path
 
 		var fileStream = cp.spawn('curl', [ 'localhost:8080/upload', '-F', 'lolwut=@' + filePath ]).stdout
 
 		fileStream.pipe(sizeStream()).once('size', function (bytes) {
-			t.equal(expectedMp3FileSize[2], bytes)
+			t.equal(expectedMp3FileSize.wav, bytes)
 			next()
 		})
 		hasha.fromStream(fileStream, { algorithm: 'md5' }).then(function (md5) {
-			t.equal(expectedMp3FileMd5s[2], md5)
+			t.equal(expectedMp3FileMd5s.wav, md5)
 			next()
 		})
 	})
@@ -42,7 +42,7 @@ test('wav [using client]', function (t) {
 	var upload = Upload()
 
 	setupServer(t, 2, function (next) {
-		var filePath = files[2].path
+		var filePath = files.wav.path
 		var fileBuffer = fs.readFileSync(filePath)
 		fileBuffer.name = filePath
 
@@ -50,11 +50,11 @@ test('wav [using client]', function (t) {
 
 		upload.on('ready', function (fileStream) {
 			fileStream.pipe(sizeStream()).once('size', function (bytes) {
-				t.equal(expectedMp3FileSize[2], bytes)
+				t.equal(expectedMp3FileSize.wav, bytes)
 				next()
 			})
 			hasha.fromStream(fileStream, { algorithm: 'md5' }).then(function (md5) {
-				t.equal(expectedMp3FileMd5s[2], md5)
+				t.equal(expectedMp3FileMd5s.wav, md5)
 				next()
 			})
 		})
